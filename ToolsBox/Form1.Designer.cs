4fd1fcb7debea07173348a7994657afd832cbefc
Alvin <mingjie831214@gmail.com>
2020-06-14 19:12:14 +0800

    完成台灣雲端書庫下載功能

--- ToolsBox/Form1.Designer.cs
+++ ToolsBox/Form1.Designer.cs
@@ -33,9 +33,23 @@
             this.iconButton4 = new FontAwesome.Sharp.IconButton();
             this.iconButton3 = new FontAwesome.Sharp.IconButton();
             this.iconButton2 = new FontAwesome.Sharp.IconButton();
-            this.iconButton1 = new FontAwesome.Sharp.IconButton();
+            this.btnEBookService = new FontAwesome.Sharp.IconButton();
             this.panelLogo = new System.Windows.Forms.Panel();
+            this.txtBookID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtToken = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBookToken = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBookName = new System.Windows.Forms.TextBox();
+            this.txtMin = new System.Windows.Forms.NumericUpDown();
+            this.txtMax = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnDownload = new System.Windows.Forms.Button();
             this.panelMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtMin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtMax)).BeginInit();
             this.SuspendLayout();
             // 
             // panelMenu
@@ -45,7 +59,7 @@
             this.panelMenu.Controls.Add(this.iconButton4);
             this.panelMenu.Controls.Add(this.iconButton3);
             this.panelMenu.Controls.Add(this.iconButton2);
-            this.panelMenu.Controls.Add(this.iconButton1);
+            this.panelMenu.Controls.Add(this.btnEBookService);
             this.panelMenu.Controls.Add(this.panelLogo);
             this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
             this.panelMenu.Location = new System.Drawing.Point(0, 0);
@@ -122,6 +136,7 @@
             this.iconButton2.FlatAppearance.BorderSize = 0;
             this.iconButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.iconButton2.Flip = FontAwesome.Sharp.FlipOrientation.Normal;
+            this.iconButton2.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Bold);
             this.iconButton2.ForeColor = System.Drawing.Color.Gainsboro;
             this.iconButton2.IconChar = FontAwesome.Sharp.IconChar.UserSecret;
             this.iconButton2.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
@@ -137,27 +152,27 @@
             this.iconButton2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.iconButton2.UseVisualStyleBackColor = true;
             // 
-            // iconButton1
+            // btnEBookService
             // 
-            this.iconButton1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.iconButton1.FlatAppearance.BorderSize = 0;
-            this.iconButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.iconButton1.Flip = FontAwesome.Sharp.FlipOrientation.Normal;
-            this.iconButton1.Font = new System.Drawing.Font("新細明體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.iconButton1.ForeColor = System.Drawing.Color.Gainsboro;
-            this.iconButton1.IconChar = FontAwesome.Sharp.IconChar.UserSecret;
-            this.iconButton1.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.iconButton1.IconSize = 32;
-            this.iconButton1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.iconButton1.Location = new System.Drawing.Point(0, 140);
-            this.iconButton1.Name = "iconButton1";
-            this.iconButton1.Rotation = 0D;
-            this.iconButton1.Size = new System.Drawing.Size(220, 60);
-            this.iconButton1.TabIndex = 1;
-            this.iconButton1.Text = "台灣雲端書庫";
-            this.iconButton1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.iconButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.iconButton1.UseVisualStyleBackColor = true;
+            this.btnEBookService.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnEBookService.FlatAppearance.BorderSize = 0;
+            this.btnEBookService.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEBookService.Flip = FontAwesome.Sharp.FlipOrientation.Normal;
+            this.btnEBookService.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnEBookService.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnEBookService.IconChar = FontAwesome.Sharp.IconChar.UserSecret;
+            this.btnEBookService.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnEBookService.IconSize = 32;
+            this.btnEBookService.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEBookService.Location = new System.Drawing.Point(0, 140);
+            this.btnEBookService.Name = "btnEBookService";
+            this.btnEBookService.Rotation = 0D;
+            this.btnEBookService.Size = new System.Drawing.Size(220, 60);
+            this.btnEBookService.TabIndex = 1;
+            this.btnEBookService.Text = "台灣雲端書庫";
+            this.btnEBookService.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEBookService.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnEBookService.UseVisualStyleBackColor = true;
             // 
             // panelLogo
             // 
@@ -167,16 +182,156 @@
             this.panelLogo.Size = new System.Drawing.Size(220, 140);
             this.panelLogo.TabIndex = 0;
             // 
+            // txtBookID
+            // 
+            this.txtBookID.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtBookID.Location = new System.Drawing.Point(461, 114);
+            this.txtBookID.Name = "txtBookID";
+            this.txtBookID.Size = new System.Drawing.Size(264, 39);
+            this.txtBookID.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(300, 114);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 30);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "bookId";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(300, 180);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 30);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "token";
+            // 
+            // txtToken
+            // 
+            this.txtToken.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtToken.Location = new System.Drawing.Point(461, 180);
+            this.txtToken.Name = "txtToken";
+            this.txtToken.Size = new System.Drawing.Size(264, 39);
+            this.txtToken.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(300, 250);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(141, 30);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "bookToken";
+            // 
+            // txtBookToken
+            // 
+            this.txtBookToken.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtBookToken.Location = new System.Drawing.Point(461, 250);
+            this.txtBookToken.Name = "txtBookToken";
+            this.txtBookToken.Size = new System.Drawing.Size(264, 39);
+            this.txtBookToken.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(300, 46);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 30);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "書名";
+            // 
+            // txtBookName
+            // 
+            this.txtBookName.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtBookName.Location = new System.Drawing.Point(461, 46);
+            this.txtBookName.Name = "txtBookName";
+            this.txtBookName.Size = new System.Drawing.Size(264, 39);
+            this.txtBookName.TabIndex = 7;
+            // 
+            // txtMin
+            // 
+            this.txtMin.Font = new System.Drawing.Font("微軟正黑體", 18F);
+            this.txtMin.Location = new System.Drawing.Point(461, 318);
+            this.txtMin.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.txtMin.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.txtMin.Name = "txtMin";
+            this.txtMin.Size = new System.Drawing.Size(120, 39);
+            this.txtMin.TabIndex = 9;
+            this.txtMin.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // txtMax
+            // 
+            this.txtMax.Font = new System.Drawing.Font("微軟正黑體", 18F);
+            this.txtMax.Location = new System.Drawing.Point(605, 318);
+            this.txtMax.Name = "txtMax";
+            this.txtMax.Size = new System.Drawing.Size(120, 39);
+            this.txtMax.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.Location = new System.Drawing.Point(300, 320);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 30);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "頁數";
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnDownload.Location = new System.Drawing.Point(605, 388);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(120, 44);
+            this.btnDownload.TabIndex = 12;
+            this.btnDownload.Text = "下載";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtMax);
+            this.Controls.Add(this.txtMin);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtBookName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtBookToken);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtToken);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBookID);
             this.Controls.Add(this.panelMenu);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "工具箱";
             this.panelMenu.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.txtMin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtMax)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -184,11 +339,23 @@
 
         private System.Windows.Forms.Panel panelMenu;
         private System.Windows.Forms.Panel panelLogo;
-        private FontAwesome.Sharp.IconButton iconButton1;
+        private FontAwesome.Sharp.IconButton btnEBookService;
         private FontAwesome.Sharp.IconButton iconButton5;
         private FontAwesome.Sharp.IconButton iconButton4;
         private FontAwesome.Sharp.IconButton iconButton3;
         private FontAwesome.Sharp.IconButton iconButton2;
+        private System.Windows.Forms.TextBox txtBookID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtToken;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBookToken;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBookName;
+        private System.Windows.Forms.NumericUpDown txtMin;
+        private System.Windows.Forms.NumericUpDown txtMax;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnDownload;
     }
 }
 
--- ToolsBox/Form1.cs
+++ ToolsBox/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FontAwesome.Sharp;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +8,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Configuration;
+using System.IO;
+using System.Drawing.Imaging;
 
 namespace ToolsBox
 {
     public partial class Form1 : Form
     {
+        string Url = ConfigurationManager.AppSettings["台灣雲端書庫網址"];
+        string f = "jpg";
+        string r = "150";
+        string preferWidth = "757";
+        string preferHeight = "1600";
+        
+        WebClient webClient=new WebClient();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            string[] paramsData = new string[] { "f=" + f, r, "preferWidth=" + preferWidth, "preferHeight=" + preferHeight, "bookToken=" + txtBookToken.Text, "token=" + txtToken.Text, "bookId=" + txtBookID.Text };
+            string queryString = string.Join("&", paramsData);
+            for (int i = (int)txtMin.Value; i <= (int)txtMax.Value; i++)
+            {
+                byte[] data = webClient.DownloadData(Url + i + "&" + queryString);
+                MemoryStream ms = new MemoryStream(data);
+                Image img = Image.FromStream(ms);
+                img.Save($@"D:\{txtBookName.Text.Trim()}\" + i + ".jpg");
+            }
+            MessageBox.Show("完成");
         }
     }
 }
